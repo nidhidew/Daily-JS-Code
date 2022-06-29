e1dc4b3f4d19ce37dddc91e2b98d2fdb8c54e8e0
nidhidew <nidhicec97@gmail.com>
2022-06-30 01:44:13 +0530

    day_2

---
+++ day_1/incr_decr.js
@@ -0,0 +1,5 @@
+var i = 5;
+i++; /**its equal too i+1 */
+
+var j = 4;
+j--; /**its equal too j-1 */
---
+++ day_1/literal_quote.js
@@ -0,0 +1,6 @@
+const myStr = "I am a \"double quoted\" string inside \"double quotes\""; 
+console.log(myStr);
+/** out put 
+ * 
+ * I am a "double quoted" string inside "double quoted".
+ */
