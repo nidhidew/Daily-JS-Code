a5de08ba8c7a0420952c162a266fbe6c4d8b9c46
nidhidew <nidhicec97@gmail.com>
2022-07-17 22:00:10 +0530

    day_6 extended about objects

--- day_6/objects.js
+++ day_6/objects.js
@@ -77,4 +77,109 @@ const myDog4 = {
   // Only change code below this line
   delete myDog4.tails;  
 
-  
+//using objects for lookups
+const alpha = {
+  1:"Z",
+  2:"Y",
+  3:"X",
+  4:"W",
+  24:"C",
+  25:"B",
+  26:"A"
+};
+
+// const thirdLetter = alpha[2];
+// const lastLetter = alpha[24];
+
+const value = 2;
+const valueLookup = alpha[value];  
+console.log(valueLookup+" valuelookup");
+
+//another example
+function phoneticLookup(val) {
+  let result = "";
+  var lookup = {
+    "alpha": "Adams",
+    "bravo":"Boston",
+    "charlie":"Chicago",
+    "delta":"Denver",
+    "echo":"Easy",
+    "foxtrot":"Frank",
+  }
+  result = lookup[val];
+  return result;
+}
+
+//testing objects for properties
+function checkObj(obj, checkProp) {
+  if(obj.hasOwnProperty(checkProp)){
+    return obj[checkProp];
+  }
+  else{
+    return "Not Found";  }
+}
+
+//manuplating complex objects
+const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+    "artist": "kumar sanu",
+    "title": "pehla pyar",
+    "release_year": 2001,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+  }
+];
+
+// accessing nested objects
+/**Access the myStorage object and assign the contents of the glove box property to the gloveBoxContents 
+ * variable.  */
+const myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
+
+const gloveBoxContents = myStorage.car.inside["glove box"] ;
+
+
+//accessing nested arrays
+const myPlants = [
+  {
+    type: "flowers",
+    list: [
+      "rose",
+      "tulip",
+      "dandelion"
+    ]
+  },
+  {
+    type: "trees",
+    list: [
+      "fir",
+      "pine",
+      "birch"
+    ]
+  }
+];
+
+const secondTree = myPlants[1].list[1];
