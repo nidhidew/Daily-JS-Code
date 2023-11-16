3f1ccd873c2458f24087337671576583bb9f6a47
Nidhi Dewangan <41151235+nidhidew@users.noreply.github.com>
2023-11-16 16:10:38 +0530

    Delete day_1 directory

--- day_1/incr_decr.js
+++
@@ -1,5 +0,0 @@
-var i = 5;
-i++; /**its equal too i+1 */
-
-var j = 4;
-j--; /**its equal too j-1 */
--- day_1/literal_quote.js
+++
@@ -1,6 +0,0 @@
-const myStr = "I am a \"double quoted\" string inside \"double quotes\""; 
-console.log(myStr);
-/** out put 
- * 
- * I am a "double quoted" string inside "double quoted".
- */
